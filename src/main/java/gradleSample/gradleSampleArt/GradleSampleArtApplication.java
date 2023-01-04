19d8f0b9da3ffa7e6aa7a34cf8c4ea21aafa66e5
jai <jai@gmail.com>
2023-01-04 17:43:29 +0530

    First commit

---
+++ src/main/java/gradleSample/gradleSampleArt/GradleSampleArtApplication.java
@@ -0,0 +1,13 @@
+package gradleSample.gradleSampleArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSampleArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSampleArtApplication.class, args);
+	}
+
+}
